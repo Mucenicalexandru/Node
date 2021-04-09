e73352a6d285b7785d94e5d7971a0d38642e5f84
Mucenicalexandru <mucenic.alexandru@gmail.com>
2021-04-09 13:42:29 +0300

    Node modules

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative");
+require("./7-mind-grenade");
+
+sayHi("Alex");
+sayHi(names.john);
+sayHi(names.peter);
---
+++ 7-mind-grenade.js
@@ -0,0 +1,9 @@
+const num1 = 5;
+const num2 = 10;
+
+const addValue = () => {
+  console.log(`the sum is ${num1 + num2}`);
+};
+
+//if we have a function that we invoke then the `require` will call this function
+addValue();
